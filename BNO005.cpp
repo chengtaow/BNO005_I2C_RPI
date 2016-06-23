8b31031a43715bc00aa9130bb441e3ff912a7d1e
Chengtao Wang <ctwang28@gmail.com>
2016-06-23 16:41:34 -0400

    Updated the BNO005 class with only Magnet

--- BNO005.cpp
+++ BNO005.cpp
@@ -14,40 +14,40 @@ using namespace std;
 namespace RPII2C{
 
 //From the data sheet
-#define CHIPID			= 0x00
+#define CHIPID			0x00
 
 // Accelaration data register
-#define ACCXLSB			= 0x08
-#define ACCXMSB			= 0x09
-#define ACCYLSB			= 0x0A
-#define ACCYMSB			= 0x0B
-#define ACCZLSB			= 0x0C
-#define ACCZMSB			= 0x0D
+#define ACCXLSB			0x08
+#define ACCXMSB			0x09
+#define ACCYLSB			0x0A
+#define ACCYMSB			0x0B
+#define ACCZLSB			0x0C
+#define ACCZMSB			0x0D
 
 //Magnetometer data register
-#define MAGXLSB			= 0x0E
-#define MAGXMSB			= 0x0F
-#define MAGYLSB			= 0x10
-#define MAGYMSB			= 0x11
-#define MAGZLSB			= 0x12
-#define MAGZMSB			= 0x13
+#define MAGXLSB			0x0E
+#define MAGXMSB			0x0F
+#define MAGYLSB			0x10
+#define MAGYMSB			0x11
+#define MAGZLSB			0x12
+#define MAGZMSB			0x13
 
 //Gyroscope data register
-#define GYROXLSB		= 0x14
-#define GYROXMSB		= 0x15
-#define GYROYLSB		= 0x16
-#define GYROYMSB		= 0x17
-#define GYROZLSB		= 0x18
-#define GYROZMSB		= 0x19
+#define GYROXLSB		0x14
+#define GYROXMSB		0x15
+#define GYROYLSB		0x16
+#define GYROYMSB		0x17
+#define GYROZLSB		0x18
+#define GYROZMSB		0x19
 
 //Temperature data register
-#define TEMP			= 0x34
+#define TEMP			0x34
 
 //Mode register
-#define OPRMODE			= 0x3D
-#define PWRMODE			= 0x3E
-#define SYSTRIGGER		= 0x3F
-#define TEMPSOURCE		= 0x40
+#define OPRMODE			0x3D
+#define PWRMODE			0x3E
+#define SYSTRIGGER		0x3F
+#define TEMPSOURCE		0x40
 
 short BNO005::combineRegister(unsigned char msb, unsigned char lsb){
 	return ((short)msb<<8)|(short)lsb;
@@ -72,7 +72,7 @@ void BNO005::getMagnetometer(){
 	magdata[0] = (double)this->combineRegister(magvalue[1], magvalue[0])/16.0;
 	magdata[1] = (double)this->combineRegister(magvalue[3], magvalue[2])/16.0;
 	magdata[2] = (double)this->combineRegister(magvalue[5], magvalue[4])/16.0;
-	this->Acceleration = &magdata;
+	this->Acceleration = *magdata;
 	cout << "The Magnetometer value of BNO005 in X direction is: " << Acceleration[0] << '\n';
 	cout << "The Magnetometer value of BNO005 in Y direction is: " << Acceleration[1] << '\n';
 	cout << "The Magnetometer value of BNO005 in Z direction is: " << Acceleration[2] << '\n';
